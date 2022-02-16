d4e983fd5f948735fba41df799c763d3145fe060
emiliooo <1668945438@qq.com>
2022-02-16 09:30:22 +0800

    2/16

--- Project1/1.c
+++ Project1/1.c
@@ -1,11 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-#include<stdio.h>
+#include <stdio.h>
+
 
 int main()
 {
-	printf("%d\n",sizeof(int*));
-
-	char b = '1';
-	return 0;
+	char a,b;
+	while (1)
+	{
+		scanf("%c", &a);
+		if (a < 58 && a>47)
+			printf("…µ±∆ƒ„ ‰»Î ˝◊÷¡À");
+		else
+		{
+			if (a < 91 && a>64)
+				printf("%c", a + 32);
+			if (a < 123 && a>96)
+				printf("%c", a - 32);
+		}
+			while(getchar()!='\n')
+			{ }
+			
+	}
 }
